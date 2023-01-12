35532c2262377bdb4925a5b79618306b273c092c
pc <nikolcesimjanovski@gmail.com>
2023-01-12 17:58:00 +0100

    Istoto navedeno podolu

---
+++ Controllers/Admin2Controller.cs
@@ -0,0 +1,171 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using NoWait.Data;
+using NoWait.Models;
+
+namespace NoWait.Controllers
+{
+    public class Admin2Controller : Controller
+    {
+        private readonly NoWaitContext _context;
+
+        public Admin2Controller(NoWaitContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin2
+        public async Task<IActionResult> Index()
+        {
+              return _context.Reservations != null ? 
+                          View(await _context.Reservations.Where(i => i.IsFinished == true)
+                              .Include(i => i.Orders)
+                              .Include(i => i.Table)
+                              .ToListAsync()) :
+                          Problem("Entity set 'NoWaitContext.Reservations'  is null.");
+        }
+
+        // GET: Admin2/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Reservations == null)
+            {
+                return NotFound();
+            }
+
+            var reservation = await _context.Reservations
+                .Where(m => m.ReservationId == id)
+                .Include(i => i.Orders).ThenInclude(i => i.Item)
+                .Include(i => i.Table).
+                FirstAsync();
+            
+            // var Orders = await  _context.Orders.Where(i => i.OrderId)
+            if (reservation == null)
+            {
+                return NotFound();
+            }
+
+            return View(reservation);
+        }
+
+        // GET: Admin2/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin2/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ReservationId,Year,Month,Day,Hour,IsFinished")] Reservation reservation)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(reservation);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(reservation);
+        }
+
+        // GET: Admin2/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Reservations == null)
+            {
+                return NotFound();
+            }
+
+            var reservation = await _context.Reservations.FindAsync(id);
+            if (reservation == null)
+            {
+                return NotFound();
+            }
+            return View(reservation);
+        }
+
+        // POST: Admin2/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ReservationId,Year,Month,Day,Hour,IsFinished")] Reservation reservation)
+        {
+            if (id != reservation.ReservationId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(reservation);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ReservationExists(reservation.ReservationId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(reservation);
+        }
+
+        // GET: Admin2/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Reservations == null)
+            {
+                return NotFound();
+            }
+
+            var reservation = await _context.Reservations
+                .FirstOrDefaultAsync(m => m.ReservationId == id);
+            if (reservation == null)
+            {
+                return NotFound();
+            }
+
+            return View(reservation);
+        }
+
+        // POST: Admin2/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Reservations == null)
+            {
+                return Problem("Entity set 'NoWaitContext.Reservations'  is null.");
+            }
+            var reservation = await _context.Reservations.FindAsync(id);
+            if (reservation != null)
+            {
+                _context.Reservations.Remove(reservation);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ReservationExists(int id)
+        {
+          return (_context.Reservations?.Any(e => e.ReservationId == id)).GetValueOrDefault();
+        }
+    }
+}
